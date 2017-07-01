b2ce7ac56e477dd8dd7707c57e2ae0bd94f78194
sergeyt <stodyshev@gmail.com>
2017-07-02 00:00:23 +0700

    error_code, error_messages to be https://github.com/goware/errorx compliant

--- errors.go
+++ errors.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Error struct {
-	ID      string `json:"id"`
+	Code    string `json:"error_code,omitempty"`
+	Message string `json:"error_message,omitempty"`
 	Status  int    `json:"status"`
-	Message string `json:"message,omitempty"`
-	Cause   string `json:"cause,omitempty"`
+	Cause   error  `json:"cause,omitempty"`
 }
 
 func (err *Error) Error() string {
@@ -18,63 +18,63 @@ func (err *Error) Error() string {
 func (err *Error) cause(cause error) *Error {
 	result := &Error{}
 	*result = *err
-	result.Cause = cause.Error()
+	result.Cause = cause
 	return err
 }
 
 var (
 	errBadAuthorizationHeader = &Error{
-		ID:      "AUTH-BAD-AUTHORIZATION-HEADER",
+		Code:    "AUTH-BAD-AUTHORIZATION-HEADER",
 		Status:  http.StatusUnauthorized,
 		Message: "Invalid authorization header",
 	}
 	errUnsupportedAuthScheme = &Error{
-		ID:      "AUTH-UNSUPPORTED-SCHEME",
+		Code:    "AUTH-UNSUPPORTED-SCHEME",
 		Status:  http.StatusUnauthorized,
 		Message: "Unsupported authentication scheme",
 	}
 	errInvalidToken = &Error{
-		ID:      "AUTH-INVALID-TOKEN",
+		Code:    "AUTH-INVALID-TOKEN",
 		Status:  http.StatusUnauthorized,
 		Message: "User token is invalid, please re-authenticate",
 	}
 	errInvalidIssuer = &Error{
-		ID:      "AUTH-INVALID-ISSUER",
+		Code:    "AUTH-INVALID-ISSUER",
 		Status:  http.StatusUnauthorized,
 		Message: "User token was issued from another host",
 	}
 	errInvalidClientIP = &Error{
-		ID:      "AUTH-INVALID-CLIENT-IP",
+		Code:    "AUTH-INVALID-CLIENT-IP",
 		Status:  http.StatusUnauthorized,
 		Message: "User token was issued for another IP address",
 	}
 	errNotAdmin = &Error{
-		ID:      "AUTH-NOT-ADMIN",
+		Code:    "AUTH-NOT-ADMIN",
 		Status:  http.StatusForbidden,
 		Message: "You need admin privileges to make this API call",
 	}
 	errMalformedContent = &Error{
-		ID:      "AUTH-BAD-CONTENT",
+		Code:    "AUTH-BAD-CONTENT",
 		Status:  http.StatusBadRequest,
 		Message: "Malformed content",
 	}
 	errBadCredentials = &Error{
-		ID:      "AUTH-BAD-CREDENTIALS",
+		Code:    "AUTH-BAD-CREDENTIALS",
 		Status:  http.StatusUnauthorized,
 		Message: "Invalid user credentials",
 	}
 	errUserNotFound = &Error{
-		ID:      "AUTH-USER-NOT-FOUND",
+		Code:    "AUTH-USER-NOT-FOUND",
 		Status:  http.StatusUnauthorized,
 		Message: "User not found",
 	}
 	errUnsupportedContentType = &Error{
-		ID:      "AUTH-UNSUPPORTED-CONTENT-TYPE",
+		Code:    "AUTH-UNSUPPORTED-CONTENT-TYPE",
 		Status:  http.StatusUnsupportedMediaType,
 		Message: "Unrecognized data format",
 	}
 	errEncodeTokenFailed = &Error{
-		ID:      "AUTH-ENCODE-TOKEN-FAILED",
+		Code:    "AUTH-ENCODE-TOKEN-FAILED",
 		Status:  http.StatusUnauthorized,
 		Message: "Cannot encode user token",
 	}
